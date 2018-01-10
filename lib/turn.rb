f8ef554b987798506d9cd0eda6c460cf12886cf3
Nandita Iyer <nandita27iyer@gmail.com>
2018-01-10 09:13:07 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,12 @@
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+puts "   |   |   " 
+puts "-----------"
+puts "   |   |   " 
+puts "-----------"
+puts "   |   |   "
+end
+
+def valid_move
+  input = 0
+  while input <= 8
+  
